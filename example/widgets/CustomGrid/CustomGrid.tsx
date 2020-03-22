3fad474410b3eb9b9d9dcb35be72d134df62e898
cc189 <cc189dev@gmail.com>
2020-03-22 00:30:11 -0700

    update example widgets

--- example/widgets/CustomGrid/CustomGrid.tsx
+++ example/widgets/CustomGrid/CustomGrid.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 import {withStyles, WithStyles, createStyles} from '@material-ui/core/styles';
-import {Widget, WidgetProps} from '../../../src/Widget/Widget';
+import {Widget, WidgetProps} from '../../../../src/Widget';
 import Grid from "@material-ui/core/Grid";
 
 const styles = createStyles({
@@ -33,7 +33,7 @@ class CustomGrid extends Widget<Props> {
                         {this.placeContainer('left')}
                     </Grid>
                     <Grid item xs={6}>
-                        {/*{this.placeContainer('right')}*/}
+                        {this.placeContainer('right')}
                     </Grid>
                 </Grid>
             </div>
